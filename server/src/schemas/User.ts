1c3389847abeefbeb77c5fb4d59e831a5a9e613c
3clipse <mdam0802work@gmail.com>
2025-10-21 13:17:29 -0400

    Added in Logging in activities and route to check if user is logged in

--- server/src/schemas/User.ts
+++ server/src/schemas/User.ts
@@ -1,14 +1,42 @@
-import mongoose from "mongoose";
+import mongoose, {Document} from "mongoose";
+import { ActivityType } from "../util/dailyActivitiesUtil";
+
+interface IActivity {
+  activity: ActivityType;
+  unit: number;
+  date: Date;
+}
+
+interface IUser extends Document {
+  _id: string;
+  username: string;
+  password: string;
+  email: string;
+  name: string;
+  activities: IActivity[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const activitySchema = new mongoose.Schema({
+  activity: {
+    type: String,
+    enum: Object.values(ActivityType)
+  },
+  unit: Number,
+  date: Date,
+});
+
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   email: String,
   name: String,
+  activities: { type: [activitySchema], default: [] },
   createdAt: Date,
   updatedAt: Date,
 });
-
 const User = mongoose.model("User", userSchema);
 
-export default User;
+export {User, IUser, IActivity};
