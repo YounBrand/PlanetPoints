95070c2766112637d3cd00f4249ce2f4812e5b88
3clipse <mdam0802work@gmail.com>
2025-10-29 13:33:06 -0400

    Added 2 routes for score calculation

--- server/src/routes/activitiesRoutes.ts
+++ server/src/routes/activitiesRoutes.ts
@@ -1,52 +1,71 @@
 import type { FastifyInstance } from "fastify";
 import { User } from "../schemas/User.js";
-import { isActivityType, logActivity, getActivities } from "../util/dailyActivitiesUtil.js";
+import {
+  isActivityType,
+  logActivity,
+  getActivities,
+  getDailyScore,
+} from "../util/dailyActivitiesUtil.js";
 import { verifyApiKey } from "../util/authUtil.js";
 
 const routes = async (fastify: FastifyInstance) => {
   fastify.post("/api/activities/log-daily", async (req, reply) => {
-
     const key = req.headers["x-api-key"];
-    if (!verifyApiKey(key as string)) return reply.code(403).send({ message: "Forbidden" }); 
+    if (!verifyApiKey(key as string))
+      return reply.code(403).send({ message: "Forbidden" });
 
-    const { userId, activity, unit } = req.body as { userId: string, activity: string, unit: number };
+    const { userId, activity, unit } = req.body as {
+      userId: string;
+      activity: string;
+      unit: number;
+    };
 
-    const user = await User.findOne({ _id: userId })
-    if (!user)
-      return reply.code(401).send({ message: "User not found" });
+    const user = await User.findOne({ _id: userId });
+    if (!user) return reply.code(401).send({ message: "User not found" });
 
     if (!activity)
-      return reply.code(400).send({ message: "You must specify an activity type" });
+      return reply
+        .code(400)
+        .send({ message: "You must specify an activity type" });
 
-    if (!unit) return reply.code(400).send({ message: "You must specify the units"});
+    if (!unit)
+      return reply.code(400).send({ message: "You must specify the units" });
 
-    if (!isActivityType(activity)) return reply.code(400).send({message: "Invalid activity type"});
+    if (!isActivityType(activity))
+      return reply.code(400).send({ message: "Invalid activity type" });
 
     const result = await logActivity(user._id.toString(), activity, unit);
-    if (result.success == true) return reply.code(200).send({ message: "Activity logged successfully" });
+    if (result.success == true)
+      return reply.code(200).send({ message: "Activity logged successfully" });
 
-    return reply.code(400).send({message: result.message});
+    return reply.code(400).send({ message: result.message });
   });
 
   fastify.get("/api/activities/get", async (req, reply) => {
     const key = req.headers["x-api-key"];
-    if (!verifyApiKey(key as string)) return reply.code(403).send({ message: "Forbidden" }); 
+    if (!verifyApiKey(key as string))
+      return reply.code(403).send({ message: "Forbidden" });
 
-    const {userId, activity, dateFrom, dateTo, unitFrom, unitTo} = req.query as {
-      userId: string,
-      activity: string,
-      dateFrom?: string,
-      dateTo?: string,
-      unitFrom?: string,
-      unitTo?: string,
-    }
+    const { userId, activity, dateFrom, dateTo, unitFrom, unitTo } =
+      req.query as {
+        userId: string;
+        activity: string;
+        dateFrom?: string;
+        dateTo?: string;
+        unitFrom?: string;
+        unitTo?: string;
+      };
 
-    if (!userId) return reply.code(400).send({ message: "You must provide a userId" });
+    if (!userId)
+      return reply.code(400).send({ message: "You must provide a userId" });
 
     if (!activity)
-      return reply.code(400).send({ message: "You must specify an activity type" });
+      return reply
+        .code(400)
+        .send({ message: "You must specify an activity type" });
 
-    if (!isActivityType(activity)) return reply.code(400).send({message: "Invalid activity type"});
+    if (!isActivityType(activity))
+      return reply.code(400).send({ message: "Invalid activity type" });
 
     const filterParams = {
       dateFrom: dateFrom ? new Date(dateFrom) : undefined,
@@ -57,10 +76,59 @@ const routes = async (fastify: FastifyInstance) => {
 
     const result = await getActivities(userId, activity, filterParams);
 
-    if (!result.success) return reply.code(400).send({ message: result.message });
+    if (!result.success)
+      return reply.code(400).send({ message: result.message });
 
     return reply.code(200).send(result);
-  })
+  });
+
+  fastify.get("/api/activities/calculate-daily", async (req, reply) => {
+    const key = req.headers["x-api-key"];
+    if (!verifyApiKey(key as string))
+      return reply.code(403).send({ message: "Forbidden" });
+
+    const { userId, date } = req.query as {
+      userId: string;
+      date: string;
+    };
+
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + 1);
+
+    const calculateDailyResponse = await getDailyScore(
+      userId,
+      new Date(newDate)
+    );
+    if (!calculateDailyResponse.success) {
+      return reply.code(500).send({ message: calculateDailyResponse.message });
+    }
+
+    return reply.code(200).send(calculateDailyResponse.score);
+  });
+
+  fastify.get("/api/activities/calculate-monthly", async (req, reply) => {
+    const key = req.headers["x-api-key"];
+    if (!verifyApiKey(key as string))
+      return reply.code(403).send({ message: "Forbidden" });
+
+    const { userId, date } = req.query as {
+      userId: string;
+      date: string;
+    };
+
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + 1);
+
+    const calculateDailyResponse = await getDailyScore(
+      userId,
+      new Date(newDate)
+    );
+    if (!calculateDailyResponse.success) {
+      return reply.code(500).send({ message: calculateDailyResponse.message });
+    }
+
+    return reply.code(200).send(calculateDailyResponse.score);
+  });
 };
 
 export default routes;
